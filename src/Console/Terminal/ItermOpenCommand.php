28d3b9336ff71d80d896f269589d51fb7e31ece8
vigih dev <vigihdev@gmail.com>
2025-12-03 10:55:13 +0700

    💥 2025-12-03 03:55:13

--- src/Console/Terminal/ItermOpenCommand.php
+++ src/Console/Terminal/ItermOpenCommand.php
@@ -63,13 +63,7 @@ final class ItermOpenCommand extends AbstractTerminalCommand
 
         foreach ($this->listProjectNames() as $dto) {
             if ($dto instanceof ProjectInterface && $dto->getName() === $name) {
-                $basePath = Path::join(Path::getHomeDirectory(), $dto->getRootPath());
-
-                if (!is_dir($basePath)) {
-                    $io->error(sprintf('Directory does not exist: %s', $basePath));
-                    return Command::FAILURE;
-                }
-                return $this->openWithFallback($basePath, $io);
+                return $this->openProjectInIterm($dto, $io);
             }
         }
         $io->error(sprintf('Project "%s" not found!', $name));
